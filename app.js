7e6823f8b9979fa8298c3b1a3a66670387c7fac4
Eddie Kao <eddie@5xcampus.com>
2025-12-03 13:45:19 +0800

    feat(cli): add interactive prompt loop with inquirer and loading spinner

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 import { GoogleGenAI } from "@google/genai";
+import { input } from "@inquirer/prompts";
+import ora from "ora";
 
 const ai = new GoogleGenAI({});
+const spinner = ora("思考中..");
 
-const resp = await ai.models.generateContent({
-  model: "gemini-2.5-flash",
-  contents: "你是誰？",
-});
+let question = await input({ message: "->" });
+while (question.trim() != "") {
+  if (question == "exit") {
+    break;
+  }
 
-console.log(resp.text);
+  spinner.start();
+  const resp = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: question,
+    config: {
+      systemInstruction: "回答一律使用台灣繁體中文",
+    },
+  });
+  spinner.stop();
+
+  console.log(resp.text);
+
+  question = await input({ message: "->" });
+}
